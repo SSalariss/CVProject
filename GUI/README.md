268ada33b317364bc8f963d09e5f71cbc1e27c45
BuildTools <unconfigured@null.spigotmc.org>
2025-09-21 16:23:57 +0200

    Menu (quasi) finito, successivamente verranno modificate le classi interne per rendere il codice piu' leggibile. Scelta del file gestita ed implementata. Cambio menu ancora da gestire ed implementare...

---
+++ GUI/README.md
@@ -0,0 +1,8 @@
+# Todo list della GUI
+Sfrutto questo readme come todo list:
+ - [ ] Menu principale
+ - [ ] Upload dell'immagine
+ - [ ] Schermata di caricamento
+ - [ ] Schermata finale con immagine risultato
+ - [ ] Pulsante "riprova"
+ - [ ] Suoni ed animazioni
--- GUI/strutture_dati.py
+++ GUI/strutture_dati.py
@@ -3,6 +3,7 @@ from PIL.Image import Image, Resampling
 
 import tkinter as tk
 from tkinter import PhotoImage, Event
+from tkinter import filedialog, messagebox
 
 from abc import ABC
 from abc import abstractmethod
@@ -236,21 +237,18 @@ class BackgroundCanvas(AdaptCanvas):
         super().__init__(master, image_path, thickness)
 
 
-class BorderButton(tk.Button):
+class BorderButtonABC(ABC, tk.Button):
     """
-    Crea un `tk.Button` con bordo.
+    Classe astratta per la gestione di `tk.Button` con bordo
 
-    Essendo che la classe `tk.Button` non possiede alcun metodo per
-    modificare il colore del bordo, la classe crea un `tk.Frame` ed
-    inserisce il pulsante al suo interno con un certo padding affinche'
-    simuli un bordo.
-    La classe e' un `tk.Button`, per ottenere il frame che la contiene vedi `get_frame`.
+    La classe crea le basi per la costruzione di un `tk.Button` contenuto da
+    un `tk.Frame` per la simulazione di un bordo.
 
-    Notare che la classe inserisce `master` come widget padre di `tk.Frame` e `tk.Frame` come 
-    padre di `tk.Button`, se `tk.Button` viene inserito in modo automatico dalla classe nel `tk.Frame` tramite
-    `pack`, cio' non accade con `tk.Frame`, infatti sara' poi l'utente ad inserirlo tramite `pack` o `grid` nel
-    `master`. Ricorda che per modificare il `tk.Frame` che contiene il `tk.Button` bisogna utilizzare il metodo `get_frame`
+    La classe implementa gli eventi `<Enter>` e `<Leave>` configurabili
+    dai metodi `btn_bd_on_leave`, `btn_bd_on_enter`, 
+    `btn_bg_on_leave`, `btn_bg_on_enter`, `on_enter` e infine, `on_leave`
     """
+
     # Frame attributes
     _outline: tk.Frame
     
@@ -262,9 +260,6 @@ class BorderButton(tk.Button):
     # Border attributes
     _border_color_on_enter: str
     _border_color_on_leave: str
-
-    # On click events
-    _on_click_events: set[str]
 
     def __init__(self, master=None, bd_width: Optional[int] = 1, bd_color: Optional[str] = "#FFFFFF", cnf={}, **kw) -> None:
         """
@@ -291,26 +286,35 @@ class BorderButton(tk.Button):
         **kw : `dict[str, Any] | None`
             Dizionario standard utilizzato per la configurazione della classe, vedi `tk.Button`
         """
+        # Inizializzo  e configuro il frame nel quale verra'
+        # inserito il `tk.Button`
         self._outline = tk.Frame(master, padx=bd_width, pady=bd_width, background=bd_color)
+
+        # Obbligo la grandezza del bordo del pulsante a 0
+        # e la funzione associata a `command`
         kw["borderwidth"] = kw.get("borderwidth", 0)
         kw["command"] = kw.get("command", self._on_click)
 
+        # Supercostruttore
         super().__init__(self._outline, cnf, **kw)
 
-        self._on_click_events = set()
-
+        # Configuro il colore standard del bordo
+        # durante gli eventi <Enter> e <Leave>
         self._border_color_on_enter = bd_color
         self._border_color_on_leave = bd_color
 
+        # Configuro il colore standard del background
+        # del pulsante durante gli eventi <Enter> e <Leave>
         self._button_color_on_enter = self.cget("bg")
         self._button_color_on_leave = self.cget("bg")
 
-        self.bind("<Enter>", self.on_enter)
-        self.bind("<Leave>", self.on_leave)
+        # Eseguo un binding per gli eventi
+        # <Enter> e <Leave>
+        self.bind("<Enter>", self._on_enter)
+        self.bind("<Leave>", self._on_leave)
 
+        # Inserisco il pulsante nel frame
         self.pack(expand=True, fill="both")
-
-
 
     def get_frame(self) -> tk.Frame:
         """
@@ -323,38 +327,128 @@ class BorderButton(tk.Button):
         """
         return self._outline
     
+    def _on_enter(self, _) -> None:
+        """
+        Effettua le modifiche di `background` e `bordo`
+        quando si verifica l'evento `<Enter>`
+        """
+        # Configuro il backgroun del pulsante
+        self.config(background=self._button_color_on_enter)
+        # Configuro il bordo del pulsante
+        self._outline.config(background=self._border_color_on_enter)
+
+    def _on_leave(self, _) -> None:
+        """
+        Effettua le modifiche di `background` e `bordo`
+        quando si verifica l'evento `<Leave>`
+        """
+        # Configuro il backgroun del pulsante
+        self.config(background=self._button_color_on_leave)
+        # Configuro il bordo del pulsante
+        self._outline.config(background=self._border_color_on_leave)
+    
+    def btn_bd_on_enter(self, color: str) -> None:
+        """ Modifica il colore del `bordo` quando si verifica l'evento `<Enter>` """
+        self._border_color_on_enter = color
+
+    def btn_bd_on_leave(self, color: str) -> None:
+        """ Modifica il colore del `bordo` quando si verifica l'evento `<Leave>` """
+        self._border_color_on_leave = color
+    
+    def btn_bg_on_enter(self, color: str) -> None:
+        """ Modifica il colore del `background` quando si verifica l'evento `<Enter>` """
+        self._button_color_on_enter = color
+
+    def btn_bg_on_leave(self, color: str) -> None:
+        """ Modifica il colore del `background` quando si verifica l'evento `<Leave>` """
+        self._button_color_on_leave = color
+
+    
+    
+
+
+class BorderButton(BorderButtonABC):
+    """
+    Classe specifica di un `BorderButton`
+
+    Questa classe
+    """
+    
+    # On click events
+    _on_click_events: set[str]
+
+    def __init__(self, master=None, bd_width: Optional[int] = 1, bd_color: Optional[str] = "#FFFFFF", cnf={}, **kw) -> None:
+        # Supercostruttore
+        super().__init__(master, bd_width, bd_color, cnf, **kw)
+
+        # Set per la gestione di eventi
+        self._on_click_events = set()
+    
+    
     def add_event_on_click(self, virtual_event: str):
+        """
+        Aggiunge un evento virtuale da sollevare
+        quando si clicca sul pulsante.
+        """
         self._on_click_events.add(virtual_event)
 
     def _on_click(self):
-        SoundManager.reproduce("resources\sounds\effects\mouse_click_effect.wav", False)
+        """
+        Viene chiamata al click del pulsante.
+        
+        La funzione si occupa delle chiamate interne da effettuare
+        dopo il click del pulsante, solo dopo aver raggiunto
+        uno stato interno stabile solleva tutti gli eventi virtuali.
+
+        Notare che la funzione potrebbe non sollevare alcun
+        evento virtuale in quanto non si e' raggiunta al soddisfacibilita'
+        durante la sua chiamata al metodo.
+        """
+        # Riproduco il suono associato al pulsante.
+        SoundManager.reproduce("resources\\sounds\\effects\\mouse_click_effect.wav", False)
+
+        # Ottengo il file scelto dall'utente (solo immagini consentite).
+        file = self.__choose_file__("Images", (".png", ".jpeg", ".jpg"))
+
+        # Se non abbiamo alcun file allora
+        # l'utente ha annullato la scelta: return
+        if not file: return
+        
+        # Altrimenti siamo arrivati ad uno stato
+        # interno stabile: solleva gli eventi virtuali
+        self.__raise_virtual_events__()
+
+
+    def __raise_virtual_events__(self):
+        """
+        Solleva tutti gli eventi virtuali associati
+        al pulsante.
+        """
         for event in self._on_click_events:
-            self.event_generate(event)
-
-    def btn_bd_on_enter(self, color: str):
-        self._border_color_on_enter = color
-
-    def btn_bd_on_leave(self, color: str) :
-        self._border_color_on_leave = color
-    
-    def btn_bg_on_enter(self, color: str):
-        self._button_color_on_enter = color
-
-    def btn_bg_on_leave(self, color: str):
-        self._button_color_on_leave = color
-
-    def on_enter(self, _):
-        self.config(background=self._button_color_on_enter)
-        self._outline.config(background=self._border_color_on_enter)
-
-    def on_leave(self, _):
-        self.config(background=self._button_color_on_leave)
-        self._outline.config(background=self._border_color_on_leave)
-    
+           self.event_generate(event)
 
 
+    def __choose_file__(self, name: Optional[str] = "any", valid_types: Optional[set[str]] = "*.*"):
+        """
+        Apre un filedialog che permette la scelta
+        di un file in base ai tipi di file concessi.
 
+        Parameters
+        --------------
+        name : `str` | `None`
+            Nome del tipo di file valido (es. Immagini, GIF, ...)
 
+        valid_types : `set[str]` | `None`
+            Tipi di file validi (default = any)
+        
+        Return
+        --------
+        File
+            Il file scelto dall'utente
+        """
+        types = " ".join(valid_types)
+        file = filedialog.askopenfile(initialdir="", title="Select an image", filetypes=[(name, types)])
+        return file
 
 
 
@@ -399,12 +493,16 @@ class MainMenu():
 
         Crea e configura i pulsanti del menu.
         """
+        # Creo e configuro il pulsante avete un bordo
         button = BorderButton(self._background.canvas(), 1, "#23AECA", text="Let's start!", font=("Aerial", 20))
         button.btn_bd_on_enter("#14414E")           # Border color on enter
         button.btn_bg_on_enter("#E0E0E0")           # Background color on enter
         button.config(activebackground="#CCCCCC")   # Background color on press
 
+        # Assegno il frame tramite `grid`
         button.get_frame().grid(row=1, column=1, sticky="nswe")
+
+        # Aggiungo gli eventi virtuali
         button.add_event_on_click("<<SIGMenu>>")
 
 
@@ -428,10 +526,17 @@ class Main:
         menu = MainMenu(self._main_window)
         self._main_window.bind("<<SIGMenu>>", self.__signal_from_menu__)
 
+    def __clear__(self):
+        for widget in self._main_window.winfo_children():
+            widget.destroy()
 
     def __signal_from_menu__(self, event):
-        print("Devo cambiare pagina")
+        self.__clear__()
+        self.__init_loading_menu__()
         
+
+    def __init_loading_menu__():
+        ...
         
 
 
